cd6f6fbc1fe38119dc71e1eda93a4304f7461bb1
anshuman361 <pandeyanshuman361@gmail.com>
2026-03-06 19:51:30 +0530

    vercel url added to app.js

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ mongoose
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://bikelo-backend.onrender.com",
+    origin: "https://bike-l0.vercel.app",
     credentials: true,
   }),
 );
